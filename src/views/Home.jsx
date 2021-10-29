9dd031f8207d4186a2924d22abf1502698f4e8cd
huaweiwei <harvey0379@163.com>
2021-10-29 09:16:58 +0800

    feat: home page background

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Statistic } from 'antd';
 import withMainLayout from '../hocs/withMainLayout';
+import '../assets/styles/home.less';
+import video from '../assets/videos/video.mp4';
+import userApi from '../apis/user.api';
 
 const HomeView = () => {
-  return <div>HomeView</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await userApi.search({ all: true });
+      setUsers(response);
+    })();
+  }, []);
+  return (
+    <div className="home">
+      <video src={video} autoPlay loop muted type="video/mp4" width="100%" />
+      <div className="main">
+        <Statistic title="活跃用户" value={users.length} />
+      </div>
+    </div>
+  );
 };
 
 export default withMainLayout(HomeView);
